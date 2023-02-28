8bab28e3ee8ee7bec4852d2d43033e784120b977
jmohib <jmohib@srcentre.com.au>
2023-02-28 13:07:39 +1100

    Need to write a script that reads the latest spec and copies those locally

---
+++ R/copy_specs.R
@@ -0,0 +1,17 @@
+#' ---
+#' title: Copying the specs across.
+#'
+#' author:
+#' created: 2020-03-26
+#' output:
+#'     html_document:
+#'         toc: true
+#'
+#' overview:
+#' The script copies the specs from different projects to keep the specs that the APP read updated.
+#' Update the new paths to specs in the setting.yaml and source the syntax to update the specs that the App reads
+
+
+srcproj::load_settings()
+
+
