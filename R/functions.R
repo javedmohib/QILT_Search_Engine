bcee93da5bd3288a77496d20b52bdcba21f210aa
jmohib <jmohib@srcentre.com.au>
2023-03-03 01:40:37 +1100

    Modified the find_files function to filter out zip and temporary files. Using matching_files as charactor vector instead of a dataframe

--- R/functions.R
+++ R/functions.R
@@ -26,71 +26,126 @@ remove_duplicate_cols <- function(df) {
 #' @param file_name One or more keywords from the name of the file
 #' @param match_all_name TRUE if all the values in file_name must be there in the output file.
 #' @param match_all_path If TRUE only gives results when all values in subfolder_path match
-find_files <- function(project, year, drive = "", subfolder_path = "", file_name = "", file_extension = "", match_all_name = TRUE, match_all_path = FALSE){
-  if(tolower(project) == "gosl") project <- "GOS-L"
-  if(tolower(drive) == "k"){
+# find_files <- function(project, year, drive = "", subfolder_path = "", file_name = "", file_extension = "", match_all_name = TRUE, match_all_path = FALSE){
+#   if(tolower(project) == "gosl") project <- "GOS-L"
+#   if(tolower(drive) == "k"){
+#     folder_paths <- glue::glue("K:/QILT/{project}/{year}")
+#   } else if(tolower(drive) == "z"){
+#     folder_paths <- glue::glue("z:/Consulting/Jobs/QILT/{project}/{year}")
+#   } else {
+#     folder_paths <- c(
+#       glue::glue("K:/QILT/{project}/{year}"),
+#       glue::glue("z:/Consulting/Jobs/QILT/{project}/{year}")
+#     )
+#   }
+#
+#   matching_files <- data.frame(file_path = character(),
+#                                sheet_name = character(),
+#                                stringsAsFactors = FALSE)
+#
+#   for (folder_path in folder_paths) {
+#     all_files <- dir(folder_path, recursive = TRUE)
+#     # remove the files that start with "., ~, $"  (i.e. hidden files) and exclude zip files
+#     all_files <- all_files[!grepl("^[\\.\\$~]", all_files) &
+#                              !grepl("\\.zip$", all_files, ignore.case = TRUE) &
+#                              grepl(paste0(".", file_extension), all_files, ignore.case = TRUE)] # select only those with desired extension
+#
+#     for (file in all_files){
+#       if (((match_all_name && all(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))) || # Runs when match_all_name = TRUE
+#            (!match_all_name && any(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))))){ # Runs when match_all_name = FALSE
+#         if (all(subfolder_path == "")){
+#           sheets <- tryCatch({
+#             sheets <- getSheetNames(file.path(folder_path, file))
+#             sheets
+#           }, error = function(e) {
+#             character(0)
+#           })
+#           if (length(sheets) > 0) {
+#             matching_files <- ifelse(nrow(matching_files) == 0,
+#                                      data.frame(file_path = file.path(folder_path, file),
+#                                                 sheet_name = sheets,
+#                                                 stringsAsFactors = FALSE),
+#                                      rbind(matching_files,
+#                                            data.frame(file_path = file.path(folder_path, file),
+#                                                       sheet_name = sheets,
+#                                                       stringsAsFactors = FALSE)))
+#           }
+#
+#         } else {
+#           matching_subfolder_path <- sub("/[^/]+$", "", file.path(folder_path, file))
+#           if ((!match_all_path && any(sapply(subfolder_path, function(arg) grepl(arg, matching_subfolder_path, ignore.case = TRUE)))) ||
+#               (match_all_path && all(sapply(subfolder_path, function(arg) grepl(arg, matching_subfolder_path, ignore.case = TRUE))))){
+#             sheets <- tryCatch({
+#               sheets <- openxlsx::getSheetNames(file.path(folder_path, file))
+#               sheets
+#             }, error = function(e) {
+#               character(0)
+#             })
+#             if (length(sheets) > 0
+#             ){
+#               matching_files <- rbind(matching_files,
+#                                       data.frame(file_path = file.path(folder_path, file),
+#                                                  sheet_name = sheets,
+#                                                  stringsAsFactors = FALSE))
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+#
+#   return(unique(matching_files$file_path))
+# }
+
+find_files <- function (project, year, drive = "", subfolder_path = "",
+                        file_name = "", file_extension = "", match_all_name = TRUE,
+                        match_all_path = FALSE)
+{
+  if (tolower(project) == "gosl")
+    project <- "GOS-L"
+  if (tolower(drive) == "k") {
     folder_paths <- glue::glue("K:/QILT/{project}/{year}")
-  } else if(tolower(drive) == "z"){
-    folder_paths <- glue::glue("z:/Consulting/Jobs/QILT/{project}/{year}")
-  } else {
-    folder_paths <- c(
-      glue::glue("K:/QILT/{project}/{year}"),
-      glue::glue("z:/Consulting/Jobs/QILT/{project}/{year}")
-    )
   }
-
-  matching_files <- data.frame(file_path = character(),
-                               sheet_name = character(),
-                               stringsAsFactors = FALSE)
-
+  else if (tolower(drive) == "z") {
+    folder_paths <- glue::glue("z:/Consulting/Jobs/QILT/{project}/{year}")
+  }
+  else {
+    1
+    folder_paths <- c(glue::glue("K:/QILT/{project}/{year}"),
+                      glue::glue("z:/Consulting/Jobs/QILT/{project}/{year}"))
+  }
+  matching_files <- character(0)
   for (folder_path in folder_paths) {
+
     all_files <- dir(folder_path, recursive = TRUE)
 
-    for (file in all_files){
-      if (!grepl("^[\\.\\$~]", file) && # remove the files that start with "., ~, $"  (i.e. hidden files)
-          grepl(paste0(".", file_extension), file, ignore.case = TRUE) && # select only those with desired extension
-          ((match_all_name && all(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))) || # Runs when match_all_name = TRUE
-           (!match_all_name && any(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))))){ # Runs when match_all_name = FALSE
-        if (all(subfolder_path == "")){
-          sheets <- tryCatch({
-            sheets <- getSheetNames(file.path(folder_path, file))
-            sheets
-          }, error = function(e) {
-            character(0)
-          })
-          if (length(sheets) > 0) {
-            matching_files <- ifelse(nrow(matching_files) == 0,
-                                     data.frame(file_path = file.path(folder_path, file),
-                                                sheet_name = sheets,
-                                                stringsAsFactors = FALSE),
-                                     rbind(matching_files,
-                                           data.frame(file_path = file.path(folder_path, file),
-                                                      sheet_name = sheets,
-                                                      stringsAsFactors = FALSE)))
-          }
-
-        } else {
-          matching_subfolder_path <- sub("/[^/]+$", "", file.path(folder_path, file))
-          if ((!match_all_path && any(sapply(subfolder_path, function(arg) grepl(arg, matching_subfolder_path, ignore.case = TRUE)))) ||
-              (match_all_path && all(sapply(subfolder_path, function(arg) grepl(arg, matching_subfolder_path, ignore.case = TRUE))))){
-            sheets <- tryCatch({
-              sheets <- getSheetNames(file.path(folder_path, file))
-              sheets
-            }, error = function(e) {
-              character(0)
-            })
-            if (length(sheets) > 0
-            ){
-              matching_files <- rbind(matching_files,
-                                      data.frame(file_path = file.path(folder_path, file),
-                                                 sheet_name = sheets,
-                                                 stringsAsFactors = FALSE))
-            }
+    all_files <- all_files[!grepl("^[\\.\\$~]", basename(all_files)) &
+                             grepl(paste0(".", file_extension), all_files, ignore.case = TRUE) &
+                             !grepl("zip$", all_files)]
+    for (file in all_files) {
+      if (((match_all_name && all(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))) ||
+           (!match_all_name && any(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))))) {
+        if (all(subfolder_path == "")) {
+          matching_files <- c(matching_files, file.path(folder_path,
+                                                        file))
+        }
+        else {
+          matching_subfolder_path <- sub("/[^/]+$",
+                                         "", file.path(folder_path, file))
+          if ((!match_all_path && any(sapply(subfolder_path,
+                                             function(arg) grepl(arg, matching_subfolder_path,
+                                                                 ignore.case = TRUE)))) || (match_all_path &&
+                                                                                            all(sapply(subfolder_path, function(arg) grepl(arg,
+                                                                                                                                           matching_subfolder_path, ignore.case = TRUE))))) {
+            matching_files <- c(matching_files, file.path(folder_path,
+                                                          file))
           }
         }
       }
     }
   }
 
-  return(unique(matching_files$file_path))
+  return(matching_files)
+
 }
+
