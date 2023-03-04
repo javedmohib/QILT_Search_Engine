ec81db3a2a18f57db5c6f57191ca7c27a250c916
jmohib <jmohib@srcentre.com.au>
2023-03-05 00:52:26 +1100

    Making app handle all common file formats like csv, tsx, doc etc

--- R/backup_working_app.R
+++ R/backup_working_app.R
@@ -66,56 +66,73 @@ server <- function(input, output, session) {
   })
 
   observeEvent(input$submit, {
+    # Display all the matching files in the select input
     choices <- files()
     print(choices)
     updateSelectInput(session, "file_path", choices = choices)
-  })
 
-
-  sheet_names <- reactive({
-    req(input$file_path)
-    return(getSheetNames(input$file_path))
-  })
-
-
-  observe({
-    choices <- sheet_names()
-    updateSelectInput(session, "sheet_name", choices = choices)
-  })
-
-  data <- reactive({
-    req(input$file_path, input$sheet_name)
-    sheet_name <- input$sheet_name
-    if (sheet_name != "") {
-      return(read.xlsx(input$file_path, sheet = sheet_name, colNames = TRUE, rowNames = FALSE))
+    # Hide the "Select a sheet" input if the selected file is not an xlsx file
+    if (tools::file_ext(choices[1]) != "xlsx") {
+      shinyjs::hide("sheet_name")
+    } else {
+      shinyjs::show("sheet_name")
     }
   })
 
-  output$data_table <- renderDataTable({
-    data()
+  observeEvent(input$file_path, {
+    file_path <- input$file_path
+    ext <- tools::file_ext(file_path)
+
+    if (ext %in% c("xlsx", "xlsm")) {
+      sheet_names <- getSheetNames(file_path)
+      updateSelectInput(session, "sheet_name", choices = sheet_names)
+      shinyjs::show("sheet_name")
+    } else if (ext %in% c("txt", "csv")) {
+      # Display the content of the file
+      output$data_table <- renderDataTable({
+        read.table(file_path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+      })
+      shinyjs::hide("sheet_name")
+    } else if (ext == "pdf") {
+      # Display the content of the file
+      output$text_output <- renderText({
+        pdftools::pdf_text(file_path)
+      })
+      shinyjs::hide("sheet_name")
+    } else if (ext %in% c("doc", "docx")) {
+      # Display the content of the file
+      output$text_output <- renderText({
+
+        doc_text <- read_docx(file_path)
+        text <- docx_summary(doc_text)
+        print(text)
+
+      })
+      shinyjs::hide("sheet_name")
+    } else {
+      # Show a message indicating that the selected file is not supported
+      showModal(modalDialog("The selected file is not supported. Please select a file with extension .xlsx, .xlsm, .txt, .csv, .doc, .docx or .pdf."))
+      shinyjs::hide("sheet_name")
+    }
   })
 
-  # Submit button observer
-  observeEvent(input$submit, {
-    # Retrieve the search parameters from the input fields
-    project <- input$project
-    year <- input$year
-    drive <- input$drive
-    subfolder_path <- strsplit(input$subfolder_path, ",\\s*")[[1]]
-    file_name <- strsplit(input$file_name, ",\\s*")[[1]]
-    file_extension <- input$file_extension
-    match_all_name <- input$match_all_name
-    match_all_path <- input$match_all_path
 
-    # Find the matching files using the find_files function
-    matching_files <- find_files(project, year, drive, subfolder_path, file_name, file_extension, match_all_name, match_all_path)
 
-    # Update the file path select input with the matching files
-    updateSelectInput(session, "file_path", choices = matching_files)
+
+
+
+  observeEvent(input$sheet_name, {
+    sheet_name <- input$sheet_name
+    file_path <- input$file_path
+
+    if (!is.null(sheet_name) && sheet_name != "") {
+      # Display the content of the selected sheet
+      output$data_table <- renderDataTable({
+        read.xlsx(file_path, sheet = sheet_name, colNames = TRUE, rowNames = FALSE)
+      })
+    }
   })
-
 }
-
 
 
 shinyApp(ui = ui, server = server)
