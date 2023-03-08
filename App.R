5209797b7be4d772767489f436a961b9b94445e3
jmohib <jmohib@srcentre.com.au>
2023-03-08 15:59:30 +1100

    Would be better to keep app in a dashboard format Moved the other app in R folder

--- App.R
+++ App.R
@@ -1,162 +1,79 @@
 library(openxlsx)
-library(fuzzyjoin)
 library(shiny)
 library(stringr)
-library(stringdist)
 library(magrittr)
 library(dplyr)
-library(shinythemes)
-library(bslib)
+library(shinydashboard)
+library(shinyjs)
+library(officer)
+library(pdftools)
 
-app1 <- shinyApp(
-  ui <- fluidPage(theme = bs_theme(version = 4, bootswatch = "minty"),
-                  titlePanel(h3("Variables Search Engine")),
-                  sidebarLayout(
-                    sidebarPanel(
-                      selectInput("survey", "Survey", choices = c("QILT", "Life in Australia"), selected = "GOS"),
-                      selectInput("project", "Project:", choices = c("GOS", "SES", "GOS-L", "ESS"), selected = "GOS"),
-                      selectInput("spec", "Specification:", choices = c("POP", "OP", "MOS", "Other", "ALL"), selected = "POP"),
-                      textInput("search_variable", "Search Variable:", value = "")
-                    ),
-                    mainPanel(
-                      tableOutput("result_table")
-                    )
-                  )
+
+header <- dashboardHeader(title = "QILT Search Engine")
+
+sidebar <- dashboardSidebar(
+  selectInput("project", "QILT Project",
+              choices = c("GOS", "SES", "ESS", "GOS-L")),
+  textInput("year", "Collection Year", value = "2023"),
+  textInput("subfolder_path", "Subfolder name(separated by commas)", value = "FEB"),
+  textInput("file_name", "Keywords in File Name (separated by commas)", value = "Operational"),
+  selectInput("file_extension", "File extension",
+              c("xlsx", "csv", "txt", "pdf", "docx"), selected = "xlsx"),
+  selectInput("drive", "Drive (K/Z)", choices = c("K", "Z", "Both")),
+  checkboxInput("match_all_name", "Match all keywords in file name", value = TRUE),
+  checkboxInput("match_all_path", "Match all subfolder paths", value = FALSE),
+  actionButton("submit", "Submit")
+)
+
+library(shinyjs)
+body <- dashboardBody(
+  shinyjs::useShinyjs(),
+
+  fluidRow(
+    column(12,
+           div(style = "display: flex; justify-content: space-between;",
+               div(style = "flex: 2.5;",
+                   selectInput(inputId = "file_path", label = "Select a file:", choices = NULL, width = "100%")
+               ),
+               div(style = "flex: 1;",
+                   shinyjs::hidden(selectInput(inputId = "sheet_name", label = "Select a sheet:", choices = NULL, width = "100%"))
+               ),
+               div(style = "display: flex: 1; justify-content: flex-end;",
+                   actionButton(inputId = "copy_path_btn", label = "Copy Path"),
+                   actionButton(inputId = "open_file_btn", label = "Open File"),
+                   actionButton(inputId = "open_folder_btn", label = "Open Folder")
+               )
+           )
+    )
   ),
 
-  server <- function(input, output) {
+  fluidRow(
+    column(12,
+           tabBox(id = "data_table", height = "700px", width = NULL,
+                  tabPanel("Table", dataTableOutput("table_output")),
+                  tabPanel("Text", verbatimTextOutput("text_output")),
+                  tabPanel("About",  shiny::includeMarkdown("README.md"))
+           )
+    )
+  )
 
-    observe({
-      # Require that all inputs are present
-      req(input$search_variable, input$project, input$spec)
-
-      # Find the file name that corresponds to the user's inputs
-      file_name <- list.files(file.path("lookup", input$project, input$spec))
-      print(file_name)
-
-      # If no file is found, return an error message
-      if (length(file_name) == 0) {
-        return(message("Error: file not found."))
-      } else {
-        # Get the full path to the file
-        file_path <- file.path("lookup", input$project, input$spec, file_name)
-        print(file_path)
-
-        # Read the variable data from the first sheet of the file
-        variable_df <- openxlsx::read.xlsx(file_path, 1) %>%
-          dplyr::rename_all(tolower) %>%
-          select(any_of(c("variable", "label"))) %>%
-          mutate(variable = tolower(variable))
-        dim(variable_df)
-
-        # Read the value data from the second sheet of the file
-        if(!input$spec %in% "OP"){
-          value_df <- read.xlsx(file_path, 2) %>%
-            dplyr::rename_all(tolower) %>%
-            select(any_of(c("variable", "value", "label"))) %>%
-            mutate(variable = tolower(variable))
-          dim(value_df)
-        } else{
-          value_df <- NULL
-        }
-      }
-
-      # Create a reactive function that filters the result data based on the user's inputs
-      result_filtered <- reactive({
-        # Convert the search variable to lowercase
-        search_variable <- tolower(input$search_variable)
-        result_variable <- variable_df[variable_df$variable %in% search_variable, ]
-
-        # If a matching variable is found, find any corresponding value data
-        if (nrow(result_variable) > 0) {
-          # Combine the matching results from both data frames into a list
-          result_value <- value_df[value_df$variable %in% search_variable, ]
-          if (nrow(result_value) > 0) {
-            result_filtered <- list(
-              Variable = as.list(result_variable),
-              Value = result_value)
-            # result_filtered <- bind_rows(result_variable, result_value)
-          } else {
-            # If no match is found in the values data frame, return the results from the variables data frame
-            result_filtered <- result_variable %>% rename("Variable" = "variable", "Label" = "label")
-          }
-        } else {
-          # If no match is found in the variables data frame
-          # Calculate the string distance between the search input and each variable in the variables data frame
-          variable_df$distance <- stringdist(variable_df$variable, search_variable, method = "jw", p = 0.1)
-          # retrieves the variable with the minimum string distance
-          nearest_match_variable <- variable_df$variable[which (variable_df$distance == min(variable_df$distance))]
-          result_value <- value_df[value_df$variable %in% nearest_match_variable, ]
-
-          if (nrow(result_value) > 0) {
-            result_variable <- variable_df[variable_df$variable %in% nearest_match_variable, ]
-            result_filtered <- bind_rows(result_variable, result_value)
-
-          } else {
-            result_variable <- variable_df[variable_df$variable %in% nearest_match_variable, ]
-            result_filtered <- result_variable
-
-          }
-
-        }
-      })
-
-      output$result_table <- renderTable({
-
-        result_filtered()
-      })
-    })
-  }
 )
 
 
-app2 <- shinyApp(ui <- fluidPage(
-
-  # theme = bs_theme(version = 4, bootswatch = "minty"),
-
-  theme = bs_theme(version = 4, bootswatch = "minty"),
-
-  titlePanel(h3("Files Search Engine")),
-
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("project", "Select project:",
-                  choices = c("GOS", "SES", "ESS", "GOS-L")),
-      textInput("year", "Enter collection year:", value = "2023"),
-      textInput("subfolder_path", "Enter sub-folder name of file path (separated by commas):", value = "feb"),
-      textInput("file_name", "Enter one or more keywords from file name (separated by commas):", value = "operational"),
-      selectInput("file_extension", "Select file extension:",
-                  c("xlsx", "csv", "txt", "pdf", "docx"), selected = "xlsx"),
-      selectInput("drive", "Drive to search for", choices = c("K", "Z", "Both")),
-      checkboxInput("match_all_name", "Match all keywords in file name:", value = TRUE),
-      checkboxInput("match_all_path", "Match all subfolder paths:", value = FALSE),
-      actionButton("submit", "Submit")
-    ),
 
 
-    mainPanel(
-      # Display the list of matching files and sheets
-      selectInput(inputId = "file_path", label = "Select a file:", choices = NULL),
-      selectInput(inputId = "sheet_name", label = "Select a sheet:", choices = NULL),
 
-      # Button to open the selected sheet
-      # actionButton(inputId = "open_sheet", label = "Open Sheet"),
-      # Render the data table output
-      dataTableOutput("data_table")
-    )
-  )
-),
+ui <- dashboardPage(header, sidebar, body)
 
-
-server <- function(input, output, session) {
-
+server <- shinyServer(function(input, output, session) {
+  useShinyjs()
   files <- reactive({
     # Retrieve the search parameters from the input fields
     project <- input$project
     year <- input$year
     drive <- input$drive
-    subfolder_path <- input$subfolder_path
-    file_name <- input$file_name
+    subfolder_path <- strsplit(input$subfolder_path, ",\\s*")[[1]]
+    file_name <- strsplit(input$file_name, ",\\s*")[[1]]
     file_extension <- input$file_extension
     match_all_name <- input$match_all_name
     match_all_path <- input$match_all_path
@@ -168,69 +85,159 @@ server <- function(input, output, session) {
     return(matching_files)
   })
 
+
   observeEvent(input$submit, {
+    # Display all the matching files in the select input
     choices <- files()
-    updateSelectInput(session, "file_path", choices = choices)
-  })
+    print(choices)
 
-
-  sheet_names <- reactive({
-    req(input$file_path)
-    return(getSheetNames(input$file_path))
-  })
-
-
-  observe({
-    choices <- sheet_names()
-    updateSelectInput(session, "sheet_name", choices = choices)
-  })
-
-  data <- reactive({
-    req(input$file_path, input$sheet_name)
-    sheet_name <- input$sheet_name
-    if (sheet_name != "") {
-      return(read.xlsx(input$file_path, sheet = sheet_name, colNames = TRUE, rowNames = FALSE))
+    if (length(choices) == 0) {
+      output$text_output <- renderText({
+        "No file found based on search inputs provided"
+      })
+    } else {
+      updateSelectInput(session, "file_path", choices = choices)
+      # Check if the selected file is an xlsx file and show/hide the sheet_name input accordingly
+      if (length(choices) > 0 && tools::file_ext(choices[1]) != "xlsx") {
+        shinyjs::hide("sheet_name")
+      } else {
+        shinyjs::show("sheet_name")
+      }
     }
   })
 
-  output$data_table <- renderDataTable({
-    data()
+
+
+  observeEvent(input$copy_path_btn, {
+
+    clipr::write_clip(input$file_path)
+
   })
 
-  # Submit button observer
-  observeEvent(input$submit, {
-    # Retrieve the search parameters from the input fields
-    project <- input$project
-    year <- input$year
-    drive <- input$drive
-    subfolder_path <- input$subfolder_path
-    file_name <- input$file_name
-    file_extension <- input$file_extension
-    match_all_name <- input$match_all_name
-    match_all_path <- input$match_all_path
+  observeEvent(input$open_file_btn, {
 
-    # Find the matching files using the find_files function
-    matching_files <- find_files(project, year, drive, subfolder_path, file_name, file_extension, match_all_name, match_all_path)
-
-    # Update the file path select input with the matching files
-    updateSelectInput(session, "file_path", choices = matching_files)
+    shell.exec(input$file_path)
   })
 
+  observeEvent(input$open_folder_btn, {
+
+    shell.exec(dirname(input$file_path))
+  })
+
+
+  # observeEvent(input$file_path, {
+  #   file_path <- input$file_path
+  #   ext <- tools::file_ext(file_path)
+  #
+  #   if (ext %in% c("xlsx", "xlsm")) {
+  #     sheet_names <- getSheetNames(file_path)
+  #     updateSelectInput(session, "sheet_name", choices = sheet_names)
+  #     shinyjs::show("sheet_name")
+  #   } else if (ext %in% c("txt", "csv")) {
+  #     # Display the content of the file
+  #     output$table_output <- renderDataTable({
+  #       read.table(file_path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  #     })
+  #     shinyjs::hide("sheet_name")
+  #   } else if (ext == "pdf") {
+  #     # Display the content of the file
+  #     output$text_output <- renderText({
+  #       pdftools::pdf_text(file_path)
+  #     })
+  #     shinyjs::hide("sheet_name")
+  #   } else if (ext %in% c("doc", "docx")) {
+  #     # Display the content of the file
+  #     output$text_output <- renderText({
+  #
+  #       doc_text <- read_docx(file_path)
+  #       text <- docx_summary(doc_text)
+  #       print(text)
+  #
+  #     })
+  #     shinyjs::hide("sheet_name")
+  #   } else if (ext %in% ""){
+  #
+  #     output$text_output <- renderText({
+  #
+  #       print("Text is displayed here only when a .doc or .pdf file is selected")
+  #
+  #     })
+  #
+  #   } else {
+  #     # Show a message indicating that the selected file is not supported
+  #     showModal(modalDialog("The selected file is not supported. Please select a file with extension .xlsx, .xlsm, .txt, .csv, .doc, .docx or .pdf."))
+  #     shinyjs::hide("sheet_name")
+  #   }
+  # })
+
+  observeEvent(input$file_path, {
+    file_path <- input$file_path
+    ext <- tools::file_ext(file_path)
+
+    if (ext %in% c("xlsx", "xlsm")) {
+      sheet_names <- getSheetNames(file_path)
+      updateSelectInput(session, "sheet_name", choices = sheet_names)
+      shinyjs::show("sheet_name")
+      shinyjs::show("table_tab")
+      shinyjs::hide("text_tab")
+    } else if (ext %in% c("txt", "csv")) {
+      # Display the content of the file
+      output$table_output <- renderDataTable({
+        read.table(file_path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+      })
+      shinyjs::hide("sheet_name")
+      shinyjs::show("table_tab")
+      shinyjs::hide("text_tab")
+    } else if (ext == "pdf") {
+      # Display the content of the file
+      output$text_output <- renderText({
+        pdftools::pdf_text(file_path)
+      })
+      shinyjs::hide("sheet_name")
+      shinyjs::hide("table_tab")
+      shinyjs::show("text_tab")
+    } else if (ext %in% c("doc", "docx")) {
+      # Display the content of the file
+      output$text_output <- renderText({
+        doc_text <- read_docx(file_path)
+        text <- docx_summary(doc_text)$text
+        return(paste("<pre>", text, "</pre>", sep = ""))
+      })
+      shinyjs::hide("sheet_name")
+      shinyjs::hide("table_tab")
+      shinyjs::show("text_tab")
+    } else if (ext %in% "") {
+      output$text_output <- renderText({
+        print("Text is displayed here only when a .doc or .pdf file is selected")
+      })
+      shinyjs::hide("sheet_name")
+      shinyjs::hide("table_tab")
+      shinyjs::show("text_tab")
+    } else {
+      # Show a message indicating that the selected file is not supported
+      showModal(modalDialog("The selected file is not supported. Please select a file with extension .xlsx, .xlsm, .txt, .csv, .doc, .docx or .pdf."))
+      shinyjs::hide("sheet_name")
+      shinyjs::hide("table_tab")
+      shinyjs::hide("text_tab")
+    }
+  })
+
+
+  observeEvent(input$sheet_name, {
+    sheet_name <- input$sheet_name
+    file_path <- input$file_path
+
+    if (!is.null(sheet_name) && sheet_name != "") {
+      # Display the content of the selected sheet
+      output$table_output <- renderDataTable({
+        read.xlsx(file_path, sheet = sheet_name, colNames = TRUE, rowNames = FALSE)
+      })
+    }
+  })
 }
 
 )
 
-# Combine the two apps into one app with two tabs
-ui <- shinyUI(
-  navbarPage("", #h2("QILT Search Engine"),
-             # position = c("fixed-top"),
-             tabPanel(h3("Varables"), app1),
-             tabPanel(h3("Files"), app2))
-)
 
-server <- function(input, output) {}
 
-# Run the app
 shinyApp(ui = ui, server = server)
-
-
